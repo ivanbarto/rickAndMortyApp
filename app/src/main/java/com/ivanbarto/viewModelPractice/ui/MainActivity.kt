af3a37901c9951bea786834a66dcb4a1a4d1d8a3
ivanbarto <ivanbartolelli@gmail.com>
2021-03-13 15:55:09 -0300

    basic character navigation

--- app/src/main/java/com/ivanbarto/viewModelPractice/ui/MainActivity.kt
+++ app/src/main/java/com/ivanbarto/viewModelPractice/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.ivanbarto.viewModelPractice.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.ivanbarto.viewModelPractice.R
+import java.security.AccessController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //find and setup navController for this activity
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+
+    //this is for setup the back arrow in toolbar, depending on the nav graph
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
