7c1da541f30a1d6e75f55385d1506f709805d027
ivanbarto <ivanbartolelli@gmail.com>
2021-03-20 16:28:57 -0300

    view binding in main activity impl

--- app/src/main/java/com/ivanbarto/viewModelPractice/ui/MainActivity.kt
+++ app/src/main/java/com/ivanbarto/viewModelPractice/ui/MainActivity.kt
@@ -6,16 +6,18 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.ivanbarto.viewModelPractice.R
+import com.ivanbarto.viewModelPractice.databinding.ActivityMainBinding
 import java.security.AccessController
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //find and setup navController for this activity
         navController = findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
